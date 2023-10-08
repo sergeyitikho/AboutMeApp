0cd6c7f3a65b248674b20ba70267e04171502b66
John Doe <sergey@itikho.com>
2023-10-08 21:47:09 +0300

    design

--- AboutMeApp/ViewController.swift
+++ AboutMeApp/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func loginPressed() {
+    }
+    
+    @IBAction func forgotUsernamePressed() {
+    }
+    
+    @IBAction func forgotPasswordPressed() {
+    }
+    
 }
 
